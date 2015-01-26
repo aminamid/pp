47d0bd0c607ef7accec81859dac6bdc22c915895
aminamid <aminamid@jpn-zaq50.(none)>
2015-01-26 15:14:08 +0900

    make it package

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+LICENSE = open(
+    os.path.join(os.path.dirname(__file__), 'LICENSE')).read().strip()
+
+DESCRIPTION = open(
+    os.path.join(os.path.dirname(__file__), 'README.md')).read().strip()
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+]
+
+setup(
+    name='pp',
+    version='0.1.0',
+    description='pretty print json, yaml, and python object.',
+    author='aminamid',
+    author_email='pekorinko@gmail.com',
+    url='https://github.com/aminamid/pp',
+    py_modules=['pp'],
+    keywords=['json', 'yaml','pretty', ],
+    classifiers=classifiers,
+    install_requires=[''],
+    license=LICENSE,
+    long_description=DESCRIPTION,
+    test_suite='')
